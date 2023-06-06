bedf2345ac7c5629a59f9936f90d47eb2888c83f
boon4681 <passbonthong@gmail.com>
2023-06-06 13:50:13 +0700

    vercel debugging 1

--- src/routes/image/square/[videoId].jpg/+server.ts
+++ src/routes/image/square/[videoId].jpg/+server.ts
@@ -15,28 +15,28 @@ const getImageURL = async (id: string): Promise<string | undefined> => {
 }
 
 export const GET: RequestHandler = async ({ params, fetch }) => {
-    await vips
-    if (params["videoId"]) {
-        const imageURL = await getImageURL(params["videoId"])
-        if (imageURL) {
-            // let t = performance.now()
-            const blob = await fetch(imageURL).then(a => a.arrayBuffer())
-            // console.log(`Loaded Image in ${performance.now() - t}`)
-            // t = performance.now()
-            let im = (await vips).Image.newFromBuffer(blob)
-            let size = Math.min(im.width, im.height);
-            let left = (im.width - size) / 2;
-            let top = (im.height - size) / 2;
-            im = im.crop(left, top, size, size);
-            im = im.resize(512 / size)
-            const save = im.writeToBuffer(".jpg")
-            // console.log(`Croped Image in ${performance.now() - t}`)
-            const headers = new Headers();
-            headers.set("content-Type", "image/jpeg")
-            return new Response(save, {
-                headers
-            })
-        }
-    }
+    // await vips
+    // if (params["videoId"]) {
+    //     const imageURL = await getImageURL(params["videoId"])
+    //     if (imageURL) {
+    //         // let t = performance.now()
+    //         const blob = await fetch(imageURL).then(a => a.arrayBuffer())
+    //         // console.log(`Loaded Image in ${performance.now() - t}`)
+    //         // t = performance.now()
+    //         let im = (await vips).Image.newFromBuffer(blob)
+    //         let size = Math.min(im.width, im.height);
+    //         let left = (im.width - size) / 2;
+    //         let top = (im.height - size) / 2;
+    //         im = im.crop(left, top, size, size);
+    //         im = im.resize(512 / size)
+    //         const save = im.writeToBuffer(".jpg")
+    //         // console.log(`Croped Image in ${performance.now() - t}`)
+    //         const headers = new Headers();
+    //         headers.set("content-Type", "image/jpeg")
+    //         return new Response(save, {
+    //             headers
+    //         })
+    //     }
+    // }
     return text("")
 };
