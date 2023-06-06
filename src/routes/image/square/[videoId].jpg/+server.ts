149c54ce092574ce6a30c7ea7823c4fdbd8b66c1
boon4681 <passbonthong@gmail.com>
2023-06-06 15:29:38 +0700

    moving image cropper to photon

--- src/routes/image/square/[videoId].jpg/+server.ts
+++ src/routes/image/square/[videoId].jpg/+server.ts
@@ -1,6 +1,6 @@
 import { text, type RequestHandler } from '@sveltejs/kit';
-// import Vips from 'wasm-vips';
 import { check } from '$lib/utils';
+import photon from '@silvia-odwyer/photon-node'
 
 const api = (id: string, option: string) => `http://i.ytimg.com/vi/${id}/${option}`
 
@@ -14,33 +14,36 @@ const getImageURL = async (id: string): Promise<string | undefined> => {
 }
 
 export const GET: RequestHandler = async ({ params, fetch }) => {
-    await new Promise<void>((res)=>setTimeout(res,8000))
-    // try {
-    //     await Vips()
-    // } catch (error) {
-    //     throw new Error("BOON ERROR")
-    // }
-    // if (params["videoId"]) {
-    //     const imageURL = await getImageURL(params["videoId"])
-    //     if (imageURL) {
-    //         // let t = performance.now()
-    //         const blob = await fetch(imageURL).then(a => a.arrayBuffer())
-    //         // console.log(`Loaded Image in ${performance.now() - t}`)
-    //         // t = performance.now()
-    //         let im = (await vips).Image.newFromBuffer(blob)
-    //         let size = Math.min(im.width, im.height);
-    //         let left = (im.width - size) / 2;
-    //         let top = (im.height - size) / 2;
-    //         im = im.crop(left, top, size, size);
-    //         im = im.resize(512 / size)
-    //         const save = im.writeToBuffer(".jpg")
-    //         // console.log(`Croped Image in ${performance.now() - t}`)
-    //         const headers = new Headers();
-    //         headers.set("content-Type", "image/jpeg")
-    //         return new Response(save, {
-    //             headers
-    //         })
-    //     }
-    // }
+    if (params["videoId"]) {
+        const imageURL = await getImageURL(params["videoId"])
+        if (imageURL) {
+            // let t = performance.now()
+            const blob = await fetch(imageURL).then(a => a.arrayBuffer())
+            let im = photon.PhotonImage.new_from_byteslice(new Uint8Array(blob))
+            let w = im.get_width()
+            let h = im.get_height()
+            let size = Math.min(w, h);
+            let left = (w - size) / 2;
+            let top = (h - size) / 2;
+            im = photon.crop(im, left, top, left + size, top + size);
+            im = photon.resize(im, 512, 512, 2)
+            // console.log(`Loaded Image in ${performance.now() - t}`)
+            // t = performance.now()
+            // let im = (await vips).Image.newFromBuffer(blob)
+            // let size = Math.min(im.width, im.height);
+            // let left = (im.width - size) / 2;
+            // let top = (im.height - size) / 2;
+            // im = im.crop(left, top, size, size);
+            // im = im.resize(512 / size)
+            // const save = im.writeToBuffer(".jpg")
+            // console.log(`Croped Image in ${performance.now() - t}`)
+            const save = im.get_bytes_jpeg(100)
+            const headers = new Headers();
+            headers.set("content-Type", "image/jpeg")
+            return new Response(save, {
+                headers
+            })
+        }
+    }
     return text("")
 };
